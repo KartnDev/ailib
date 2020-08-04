8887eb7633fbf3dfb8a58bcd26b0eaa3cbdaf502
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-08-04 18:24:07 +0300

    add SVC topology

--- ailib/SVMClassifier.cpp
+++ ailib/SVMClassifier.cpp
@@ -1,1 +1,46 @@
 #include "SVMClassifier.h"
+
+
+template<typename _TVal>
+SVMClassifier<_TVal>::SVMClassifier()
+{
+
+}
+
+template<class _TVal>
+SVMClassifier<_TVal>::SVMClassifier(Kernal kernalType)
+{
+}
+
+template<class _TVal>
+void SVMClassifier<_TVal>::Fit(vector<vector<_TVal>> trainData, vector<int> trainLabels)
+{
+}
+
+template<class _TVal>
+int SVMClassifier<_TVal>::Predict(vector<_TVal> testSingle)
+{
+	return 0;
+}
+
+template<class _TVal>
+double SVMClassifier<_TVal>::AssetAccuracy(vector<int> predicted, vector<int> actual)
+{
+	return 0.0;
+}
+
+template<class _TVal>
+double SVMClassifier<_TVal>::AssetAccuracy(vector<int> actual)
+{
+	return 0.0;
+}
+
+template<class _TVal>
+void SVMClassifier<_TVal>::SaveModel(string fileLocation)
+{
+}
+
+template<class _TVal>
+void SVMClassifier<_TVal>::LoadModel(string fileLocation)
+{
+}
--- ailib/SVMClassifier.h
+++ ailib/SVMClassifier.h
@@ -1,11 +1,21 @@
 #include "pch.h"
 
+enum Kernal
+{
+	linear,
+	polynomial,
+	rbf, // radial basis function
+	sigmoid
+};
+
+
 
 template <class _TVal>
 class SVMClassifier // Linear - kernal svm
 {
 public:
 	SVMClassifier();
+	SVMClassifier(Kernal kernalType);
 	void Fit(vector<vector<_TVal>> trainData, vector<int> trainLabels);
 	int Predict(vector<_TVal> testSingle);
 	vector<int> Predict(vector<vector<_TVal>> testData);
