d1144b85d84a37293ef500e75e6f6e4ad086313c
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-09-08 23:44:52 +0300

    relocate to the clion

---
+++ cmake-build-debug/CMakeFiles/ailib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ailib.dir/main.cpp.obj"
+  "ailib.exe"
+  "ailib.exe.manifest"
+  "ailib.pdb"
+  "libailib.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ailib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
