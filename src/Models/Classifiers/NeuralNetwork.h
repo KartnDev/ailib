26e19f5682487450a954103b82c9c68ca23f4890
Dmitry <43281522+KartonDCP@users.noreply.github.com>
2020-09-17 23:16:28 +0300

    add NN functionality

--- src/Models/Classifiers/NeuralNetwork.h
+++ src/Models/Classifiers/NeuralNetwork.h
@@ -4,11 +4,25 @@
 
 #ifndef AILIB_NEURALNETWORK_H
 #define AILIB_NEURALNETWORK_H
+#include <vector>
 
+#include "IClassifier.h"
 
-class NeuralNetwork
+template<class DType>
+class NeuralNetwork : IClassifier<DType>
 {
-
+public:
+    NeuralNetwork(std::vector<int> layersNeuronCount);
+    void Fit(DType** xData, int* yData, int dataSize, int featureCount) override;
+    int* Predict(DType** predictFetchData, int predictSize) const override;
+    int Predict(DType* predictVector) const override;
+    void SaveModel(std::string path) override;
+    void LoadModel(std::string path) override;
+private:
+    void Initialization();
+    void FeedForward();
+    void BackPropagation();
+    void WeightsTraining();
 };
 
 
